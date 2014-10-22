7cebde8fa7f6d93b2333d2714f28ba9a95b2e8ab
bionic <bionic@bionics-iMac.local>
2014-10-22 21:36:17 +0300

    my first test
    first test

---
+++ src/main/java/less3/testSum.java
@@ -0,0 +1,10 @@
+package less3;
+
+/**
+ * Created by bionic on 22.10.14.
+ */
+public class testSum {
+    public int sum(int a, int b){
+        return a+b;
+    }
+}
--- src/test/java/pak/FirstTest.java
+++ src/test/java/pak/FirstTest.java
@@ -1,16 +1,19 @@
 package pak;
 
+import less3.testSum;
 import org.testng.annotations.Test;
 import org.testng.Assert;
+
+import static org.testng.Assert.assertEquals;
 
 /**
  * Created by bionic on 22.10.14.
  */
 public class FirstTest {
+
     @Test
-    public void firstTest()
-    {
-        System.out.print("new print");
-        Assert.assertTrue(true);
+    public void firstTest() {
+        testSum sum = new testSum();
+        assertEquals(sum.sum(1, 2),4);
     }
 }
