90c418153973751709fe6f0c507422a3cbd8f0ae
Andrey90 <aluchkovsky90@gmail.com>
2014-11-03 16:56:41 +0200

    HomeTask 03.11.14

---
+++ src/main/java/actorsHL/UserHL.java
@@ -0,0 +1,8 @@
+package actorsHL;
+
+public class UserHL {
+    public String email;
+    public String nik;
+    public String password;
+    public String repPass;
+}
---
+++ src/main/java/pagesHL/regPage.java
@@ -0,0 +1,17 @@
+package pagesHL;
+
+import actorsHL.UserHL;
+
+
+public class regPage {
+    public void registration(UserHL userHL) {
+        checkEmail(userHL);
+        checkNik(userHL);
+    }
+
+    private void checkEmail(UserHL userHL) {
+    }
+
+    private void checkNik(UserHL userHL) {
+    }
+}
